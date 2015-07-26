9f424326a3736659a031da7731d603ec2a2ffcb4
pkafel <piotr.kafel@gmail.com>
2015-07-26 22:52:39 +0200

    FrogJmp

---
+++ src/com/piotrkafel/lesson1/FrogJmp.java
@@ -0,0 +1,11 @@
+package com.piotrkafel.lesson1;
+
+/**
+ * Count minimal number of jumps from position X to Y.
+ */
+public class FrogJmp {
+
+    public static int solution(int X, int Y, int D) {
+        return (int) Math.ceil((Y - X)/(double)D);
+    }
+}
